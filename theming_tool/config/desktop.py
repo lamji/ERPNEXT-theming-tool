9d7000e8da7dca4fc49e385712713bf1a6509c2b
frappe <frappe@ubuntu.vm>
2021-09-10 04:45:12 +0000

    first commit

---
+++ theming_tool/config/desktop.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Theming Tool",
+			"category": "Modules",
+			"color": "grey",
+			"icon": "octicon octicon-paintcan",
+			"type": "module",
+			"label": _("Theming Tool")
+		}
+	]
